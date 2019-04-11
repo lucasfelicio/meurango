acd4ae76a349710f930b439485f3a8a967f34dd2
Lucas Felicio <felicio.lucas@outlook.com>
2019-04-10 21:58:50 -0400

    Tela Cadastro de Receitas
    estagio inicial, apenas elementos sem função

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { NologinGuard } from './guards/nologin.guard';
 
   const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '', redirectTo: 'cadastro-receita', pathMatch: 'full' },
     { path: 'home', loadChildren: './home/home.module#HomePageModule' },
     { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
     { path: 'cadastro', loadChildren: './cadastro/cadastro.module#CadastroPageModule' },
+  { path: 'cadastro-receita', loadChildren: './cadastro-receita/cadastro-receita.module#CadastroReceitaPageModule' },
+
   ];
 
 @NgModule({
